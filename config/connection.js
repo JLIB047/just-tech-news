2a2170c80fd6a35ee4944af484861b73b0387d12
Jason Libertelli <jason.libertelli@gmail.com>
2021-12-12 20:59:10 -0500

    CRUD actions

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+//import the sequeliza constructor from the library 
+const Sequlize = require('sequelize');
+
+require('dotenv').config();
+
+//create connection to our database, pass in your mySQL info for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    part: 3306
+})
+
+module.exports = sequelize;
