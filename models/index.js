68dd25e227f78f19fd630fe0c359d6b9837f100c
Jason Libertelli <jason.libertelli@gmail.com>
2021-12-14 22:23:18 -0500

    post-routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post')
 
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//defining realtionship of Post model to User model
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
