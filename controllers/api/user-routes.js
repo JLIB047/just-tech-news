835f6236f16a193246f06ad9106d368342ed8a14
Jason Libertelli <jason.libertelli@gmail.com>
2021-12-29 13:12:54 -0500

    404 not found Post req

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,23 +55,68 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
-      });
+    }
+    /* //User.create({
+      //username: req.body.username,
+      //email: req.body.email,
+      //password: req.body.password
+    //})
+      //.then(dbUserData => res.json(dbUserData))
+      //.catch(err => {
+        //console.log(err);
+        //res.status(500).json(err);
+      //}); */
   });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-    User.findOne({
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if(!dbUserData) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggenIn = true;
+
+            res.status(200).json({ user: dbUserData, message: 'You are logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    /* User.findOne({
       where: {
         email: req.body.email
       }
@@ -88,7 +133,7 @@ router.post('/login', (req, res) => {
       }
   
       res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+    }); */
   });
 
 
